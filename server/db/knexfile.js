9b4399edb7d2eabd1b9d9111b1619b997f2640d5
joon jeong <juny.jeong@gmail.com>
2023-10-17 23:11:47 +1300

    update knex config

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -32,15 +32,12 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      host: process.env.HOST_URL,
-      port: process.env.PORT,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-    },
-    pool: {
-      min: 2,
-      max: 10,
+      connectionString: process.env.DATABASE_URL,
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
     },
     migrations: {
       tableName: 'knex_migrations',
